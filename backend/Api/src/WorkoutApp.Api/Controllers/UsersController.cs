93515221bf0dd3d1eba782f1f0f33c6274e45178
MarkoKliska <021markokliska@gmail.com>
2026-08-02 18:45:23 +0200

    Added backend for updating user info

--- backend/Api/src/WorkoutApp.Api/Controllers/UsersController.cs
+++ backend/Api/src/WorkoutApp.Api/Controllers/UsersController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using WorkoutApp.Api.Common;
+using WorkoutApp.Application.DTOs.User.UpdateProfile;
+using WorkoutApp.Application.Features.Users.Commands.UpdateProfile;
 using WorkoutApp.Application.Features.Users.Queries.GetProfile;
 
 namespace WorkoutApp.Api.Controllers;
@@ -15,6 +17,13 @@ public sealed class UsersController(ISender sender) : ControllerBase
     public async Task<IActionResult> GetProfile(CancellationToken cancellationToken)
     {
         var result = await sender.Send(new GetProfileQuery(), cancellationToken);
+        return result.ToActionResult();
+    }
+
+    [HttpPut("me")]
+    public async Task<IActionResult> UpdateProfile(UpdateProfileRequest request, CancellationToken cancellationToken)
+    {
+        var result = await sender.Send(new UpdateProfileCommand(request), cancellationToken);
         return result.ToActionResult();
     }
 }
---
+++ backend/Api/src/WorkoutApp.Application/Features/Users/Commands/UpdateProfile/UpdateProfileCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace WorkoutApp.Application.Features.Users.Commands.UpdateProfile;
+
+public sealed class UpdateProfileCommandValidator : AbstractValidator<UpdateProfileCommand>
+{
+    public UpdateProfileCommandValidator()
+    {
+        RuleFor(x => x.Request.FirstName).NotEmpty().MaximumLength(100);
+        RuleFor(x => x.Request.LastName).NotEmpty().MaximumLength(100);
+        RuleFor(x => x.Request.Email).NotEmpty().EmailAddress().MaximumLength(320);
+    }
+}
